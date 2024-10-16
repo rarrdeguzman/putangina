535b24ef0b6d15e827e36be41f343b9fe525ce26
rarrdeguzman <157084046+rarrdeguzman@users.noreply.github.com>
2024-10-16 02:32:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+const StateTutorial = () => {
+  const [inputValue, setInputValue] = useState("Pedro");
+
+  let onChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
+  return (
+    <div>
+
+      <input placeholder="enter something..." onChange={onChange} />
+
+      <br />
+
+      {inputValue}
+      
+    </div>
+  );
+};
+
+export default StateTutorial;
